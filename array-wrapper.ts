c564106731220c22ef48cf7941e124a186672686
mostafa.mahdavian <mt.gm.computer.01@gmail.com>
2024-10-12 16:53:49 +0330

    feat: array-wrapper problem resolved using class and methods and property and baktik syntax concept.

---
+++ array-wrapper.ts
@@ -0,0 +1,25 @@
+class ArrayWrapper {
+
+    nums: number[];
+
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
+
+    valueOf(): number {
+        const sum: number = this.nums.reduce((accum, curr) => accum + curr, 0);
+        return sum;
+    }
+
+    toString(): string {
+        return `[${this.nums.toString()}]`;
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
